780fff63979d4c5cc41b7a4cb0fc0a5788dbcb01
sarthak-rawat-1101 <sarthak.rawat@students.iiit.ac.in>
2022-10-23 12:05:54 +0530

    minor changes

--- car00.py
+++ car00.py
@@ -1,23 +1,38 @@
 from cryptography.fernet import Fernet
 import mqtt
+import capture_frames
+import pickle
+import json
 
 # TO:DO use env variables
+id = "car_00"
 key = b'sDPwjSlr6uHHuH8ezqzjZBJau8dTShpPHWxwkDvVf-c='
 fernet = Fernet(key)
 
-mqtt.connect()
+mqtt.connect(id)
+
+def send_frames(topic, frames):
+    msg = {
+        "id": id,
+        "message_type": "IMAGE_STREAM",
+        "message_body": frames
+    }
+    pkl = pickle.dumps(msg)
+    mqtt.publish(topic, pkl)
+    print(pkl[50:100])
 
 def send_image(topic, file_name):
     f = open(file_name, "rb")
     filecontent = f.read()
     byteArr = fernet.encrypt(filecontent)
     msg = {
-        "id": "car_00",
+        "id": id,
         "body": byteArr
     }
-    mqtt.publish(topic, str(msg))
+    pkl = pickle.dumps(msg)
+    mqtt.publish(topic, pkl)
 
-def send_dict(topic, msg):
+def send_msg(topic, msg):
     mqtt.publish(topic, str(msg))
 
 def fetch_updates(topic):
@@ -25,14 +40,13 @@ def fetch_updates(topic):
         # zip file
         recvd_obj = eval(str(message.payload.decode("utf-8")))
         # save this file
+        with open(id + "/update_files.zip", "wb") as f:
+            f.write(recvd_obj)
         # start update routine
+        print("Downloaded update")
 
     mqtt.subscribe(topic, on_message)
 
 if __name__ == "__main__":
-    pass
-    '''
-    
-
-    
-    '''
+    # fetch_updates("updates")
+    send_frames("CLOUD_SERVER", capture_frames.capture_frames(1))
